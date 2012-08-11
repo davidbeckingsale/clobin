bbb6e3c0cf95ed4ec17f7a39bcd8db0d2b7277e5
David Beckingsale <davidbeckingsale@gmail.com>
2012-08-11 22:32:43 +0100

    Switched to noir 1.3.0 and clojure 1.4.0
    Also upgraded to Leiningen 2.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clobin "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]
+            :dependencies [[org.clojure/clojure "1.4.0"]
+                           [noir "1.3.0-beta1"]
                            [com.taoensso/carmine "0.10.1"]]
             :dev-dependencies [[org.clojars.scott/lein-nailgun "1.1.0"]
                                [vimclojure/server "2.3.3"]]
