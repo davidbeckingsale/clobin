f7c26b9fe55c407373cefa6d07e3ea512b1de2e3
David Beckingsale <davidbeckingsale@gmail.com>
2012-08-12 17:49:46 +0100

    Added SyntaxHighlighter to colour pastes

--- src/clobin/templates.clj
+++ src/clobin/templates.clj
@@ -1,19 +1,30 @@
 (ns clobin.templates
   (:use [noir.core :only [defpartial]]
-        [hiccup.page-helpers :only [include-css html5]]))
+        [hiccup.page :only [include-css include-js html5]]
+        [hiccup.util :only [escape-html]]
+        [hiccup.element :only [link-to]]))
 
 (defpartial layout [& content]
             (html5
               [:head
                [:title "clobin"]
                (include-css "/css/bootstrap.css")
-               (include-css "/css/styles.css")]
+               (include-css "/css/styles.css")
+               (include-css "/css/shCore.css")
+               (include-css "/css/shThemeDefault.css")
+               (include-js "/js/shCore.js")
+               (include-js "/js/shBrushCpp.js") ]
               [:body
                [:div.container
                 [:header
-                 [:h1 "Clobin"]
+                 [:h1 (link-to "/" "Clobin")]
                  [:h2 "a simple clojure pastebin"]]
-                content]]))
+                content
+                [:footer
+                 (link-to "/about" "About")
+                 " | Created by "
+                 (link-to "http://www.davidbeckingsale.com" "David Beckingsale")]
+                [:script "SyntaxHighlighter.all()"]]]))
 
 (defpartial index [& content]
             (layout
@@ -27,4 +38,4 @@
 
 (defpartial paste [& content]
             (layout
-              [:pre content]))
+              [:pre {:class "brush: cpp"} content]))
